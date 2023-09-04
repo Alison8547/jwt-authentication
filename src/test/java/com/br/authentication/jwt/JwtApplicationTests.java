a170598e2f72ead428efe47bcba4e78402f129f3
Alison8547 <alisonailson100@hotmail.com>
2023-09-04 15:43:33 -0300

    feat: project base

---
+++ src/test/java/com/br/authentication/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.authentication.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
